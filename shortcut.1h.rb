0c79be94bf6cbbc126c2fbbb03397dbe8df4795e
Madeline Vogel <madmvogel@gmail.com>
2023-02-14 15:52:50 -0600

    completed xbar shortcut plugin

---
+++ shortcut.1h.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env /Users/madelinevogel/.rbenv/shims/ruby
+
+puts "📌"
+puts "---"
+puts "GitHub Home | font=UbuntuMono-Bold | href=https://github.com/madelinevogel"
+puts "--" "Actualize Resources | href=https://github.com/actualize-daytime-2022-12-04/resources"
+puts "--" "My Repositories | href=https://github.com/madelinevogel?tab=repositories"
+puts "Gmail Home | href=https://mail.google.com/mail/u/0/#inbox"
+puts "--" "Google Drive | href=https://drive.google.com/drive/u/0/my-drive"
+puts "Word of the Day | href=https://www.merriam-webster.com/word-of-the-day"
